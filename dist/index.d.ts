dab16ea3249161a547205103f4d4f505234f5711
Paul Jones <paul.jones@creditplus.co.uk>
2018-12-12 10:05:26 +0000

    deleted needless file.

--- dist/index.d.ts
+++
@@ -1,6 +0,0 @@
-export * from "./operation/base";
-export * from "./operation/http";
-export * from "./operation/json";
-export * from "./error/operationInvocation";
-export * from "./error/serialisation";
-export * from "./error/validation";
