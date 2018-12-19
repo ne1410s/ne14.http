d142478ad5ccb0b582048a77bc1e805b8cabfccc
ne1410s <pgj646@gmail.com>
2018-12-19 21:27:48 +0000

    Streamlining of compilation..

--- test/example.js
+++ test/example.js
@@ -1,12 +1,8 @@
-const expect = require('chai').expect;
-
-const ne14 = {
-    http: require('../dist/index')
-};
+const ne14_http = require('../dist/index');
 
 describe('#example', () => {
 
     it('should ...', () => {
-        
+        console.log(ne14_http);
     });
 });
